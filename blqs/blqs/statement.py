5e4a6b1ed15f76f36c4b1d83d9d623d1025ce1bd
Dave Bacon <dabacon@gmail.com>
2021-03-26 11:00:30 -0700

    fix ci

--- blqs/blqs/statement.py
+++ blqs/blqs/statement.py
@@ -18,5 +18,6 @@ class Statement:
 
     def __init__(self):
         # When in a block context, always append the statement on creation.
-        if (current_block := block_stack.get_current_block()) is not None:
+        current_block = block_stack.get_current_block()
+        if current_block is not None:
             current_block.append(self)

52c751e030c75761ca72c031a728e738826f4e5e
Dave Bacon <dabacon@gmail.com>
2021-04-21 21:03:02 -0700

    Add more gates

--- blqs_cirq/blqs_cirq/gates.py
+++ blqs_cirq/blqs_cirq/gates.py
@@ -13,6 +13,9 @@ class CirqBlqsOp(blqs.Op):
         return self._gate
 
 
+    def __doc__(self):
+
+
 class CirqBlqsOpFactory:
     def __init__(self, cirq_gate_class):
         self._cirq_gate_class = cirq_gate_class
@@ -37,5 +40,27 @@ class Gates:
             return self._dispatch[item]
 
 
+# Single qubit gate constants.
+X = cirq_blqs_op(cirq.X)
+Y = cirq_blqs_op(cirq.Y)
+Z = cirq_blqs_op(cirq.Z)
 H = cirq_blqs_op(cirq.H)
+S = cirq_blqs_op(cirq.S)
+T = cirq_blqs_op(cirq.T)
+
+# Single qubit gate classes.
+XPowGate = cirq_blqs_op(cirq.XPowGate)
+YPowGate = cirq_blqs_op(cirq.YPowGate)
+ZPowGate = cirq_blqs_op(cirq.ZPowGate)
 HPowGate = cirq_blqs_op(cirq.HPowGate)
+PhasedXPowGate = cirq_blqs_op(cirq.PhasedXPowGate)
+
+# Two qubit gate classes
+CZ = cirq_blqs_op(cirq.CZ)
+CNOT = CX = cirq_blqs_op(cirq.CNOT)
+SWAP = cirq_blqs_op(cirq.SWAP)
+ISWAP = cirq_blqs_op(cirq.ISWAP)
+XX = cirq_blqs_op(cirq.XX)
+YY = cirq_blqs_op(cirq.YY)
+ZZ = cirq_blqs_op(cirq.ZZ)
+

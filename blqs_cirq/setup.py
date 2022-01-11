7d4e59aa3baae85e6d5ba1b425588cd87708403d
Dave Bacon <dabacon@gmail.com>
2022-01-11 20:34:50 +0000

    Test out pypi and update instructions

--- blqs_cirq/setup.py
+++ blqs_cirq/setup.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 import io
-from setuptools import setup
+from setuptools import find_packages, setup
 
 # Read the __version__.
 __version__ = ""
@@ -30,8 +30,8 @@ long_description = io.open("README.md", encoding="utf-8").read()
 requirements = open("requirements.txt").readlines()
 requirements = [r.strip() for r in requirements]
 
-packages = [
-    "blqs_cirq",
+packages = ["blqs_cirq"] + [
+    "blqs_cirq." + package for package in find_packages(where="blqs_cirq")
 ]
 
 setup(
@@ -39,12 +39,35 @@ setup(
     version=__version__,
     url="https://github.com/ionq/blqs_cirq",
     author="The Blqs Developers",
-    author_email="bacon@ionq.com",
+    author_email="dabacon@gmail.com",
     python_requires=(">=3.7.0"),
     install_requires=requirements,
-    licence="Apache 2",
+    license="Apache 2",
     description=description,
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=packages,
 )
+
+
+# Instruction for release
+# 1. Create distribution:
+#   python setup.py sdist bdist_wheel
+# 2. Check the distribution:
+#   twine check dist/*
+# 3. Upload to test pypi:
+#   twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+# 4. Visit https://test.pypi.org/project/blqs_cirq/ and check that it can install.
+# You will need to install requirement since most of these are not on test pypi:
+#   pip install -r ../blqs/requirements.txt
+#   # Assuming that you have just also updated blqs do
+#   pip install -i https://test.pypi.org/simple/ blqs
+#   # Else
+#   pip install blqs
+#   # Finally
+#   pip install -r requirements.txt
+# Followed by pip instructions from website
+#   pip install -i https://test.pypi.org/simple/ blqs_cirq
+# 5. Upload to prod pypi:
+#   twine upload di    st/*
+# 6. Confirm on https://pypi.org/project/blqs/ and test install using pip.
